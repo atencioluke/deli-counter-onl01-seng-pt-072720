5b94984a894e529465441d911dcc3c21942bc184
Luke Atencio <atencioluke@gmail.com>
2020-07-29 04:54:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+def line(deli)
+  if deli.empty?
+    puts "The line is currently empty."
+  else
+    current_line = "The line is currently:"
+    deli.each.with_index(1) { |name, index|
+      current_line << " #{index}. #{name}"
+    }
+    puts current_line
+  end
+end
+
+def take_a_number(deli, new_name)
+  deli << new_name
+  puts "Welcome, #{new_name}. You are number #{deli.index + 1} in line."
+end
